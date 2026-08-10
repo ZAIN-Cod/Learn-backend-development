a4864d60bd64aad51b8649bb237b7878386704c3
ZAIN-Cod <2025se405@student.uet.edu.pk>
2026-08-10 11:22:50 +0500

    all setup configured

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ dns.setServers(['8.8.8.8', '8.8.4.4']);
 //require('dotenv').config({path: './.env'})
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js"
+import { app } from './app.js';
 
 dotenv.config({
     path: './.env'
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import {v2 as cloudinary} from 'cloudinary'
+
+import fs from 'fs'
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+
+const uploadCloudinary = async (localFilePath) => {
+    try{
+if(!localFilePath) return null
+
+//upload file to cloudinary
+
+const response=await cloudinary.uploader.upload(localFilePath,{
+    resource_type: 'auto',
+}) 
+//file has been uploaded successfuly
+console.log("file uploaded to cloudinary successfully", response.url);
+return response;
+    }
+    catch (error) {
+        fs.unlinkSync(localFilePath)//remove the locallly saved temorary file
+        //as the upload operation got falied
+        return null
+
+    }
+}
+
+export {uploadCloudinary}
